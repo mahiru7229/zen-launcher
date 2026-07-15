6db849d75ca38e2b82461d867e9ad2bcb5d278ea
Huỳnh Ngọc Hoàng Tùng <110292918+mahiru7229@users.noreply.github.com>
2026-07-16 02:20:16 +0700

    fix: fixing version mismatch

--- src/config.py
+++ src/config.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
-VERSION = "v0.5.1 Beta 2"
-VERSION_ID = "0.5.1-beta.2"
+VERSION = "v0.5.1 Beta 3"
+VERSION_ID = "0.5.1-beta.3"
 VERSION_TAG = f"v{VERSION_ID}"
 UPDATE_CHANNEL = "beta"
 GITHUB_REPOSITORY = "mahiru7229/mcw-launcher"
